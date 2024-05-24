532866a71eb0ce3c6afc67b07c627efa8ade3c98
weiweicao <weiwei.cao@mdland.com>
2024-05-24 09:24:30 -0400

    feat: setup plugins for content script

--- ext-demo/readme.md
+++ ext-demo/readme.md
@@ -9,3 +9,17 @@ Manually add a manifest.json file to under the /public folder
 Build the project: `yarn build` and
 
 Load the `dist` folder to run the extension
+
+
+## Injected extension
+
+using content scripts to inject a React application into the host page
+
+
+https://dev.to/bnn1/mise-en-place-31n5
+
+
+
+```
+yarn add -D vite-tsconfig-paths @crxjs/vite-plugin@beta
+```
--- ext-demo/vite.config.ts
+++ ext-demo/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
+import tsconfigPaths from "vite-tsconfig-paths";
+import { ManifestV3Export, crx } from "@crxjs/vite-plugin";
+
+import manifestJson from "./public/manifest.json";
+
+const manifest = manifestJson as ManifestV3Export;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [preact(),tsconfigPaths(),crx({ manifest })],
 })
